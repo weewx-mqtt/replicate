adf44153894c0e13d3ae4a965a44d58720ab2c0a
Rich Bell <bellrichm@gmail.com>
2026-01-29 10:19:27 -0500

    MQTTResponderLoopThread tests

--- bin/user/tests/unit/test_mqttresponderloopthread.py
+++ bin/user/tests/unit/test_mqttresponderloopthread.py
@@ -12,9 +12,112 @@ import mock
 
 import helpers
 
+import random
+
 import user.mqttreplicate
 
-class TestInit(unittest.TestCase):
+class TestMQTTResponderLoopThread(unittest.TestCase):
+    def test_init(self):
+        mock_logger = mock.Mock()
+        mock_client = mock.Mock()
+        host = helpers.random_string()
+        port = helpers.random_string()
+        keepalive = helpers.random_string()
+
+        with mock.patch('user.mqttreplicate.threading'):
+            with mock.patch('user.mqttreplicate.paho.mqtt'):
+
+                SUT = user.mqttreplicate.MQTTResponderLoopThread(mock_logger,
+                                                                 mock_client,
+                                                                 None,
+                                                                 True,
+                                                                 None,
+                                                                 None,
+                                                                 None,
+                                                                 None,
+                                                                 host,
+                                                                 port,
+                                                                 keepalive)
+
+                self.assertEqual(mock_client.on_connect, SUT._on_connect)
+                self.assertEqual(mock_client.on_message, SUT._on_message)
+                self.assertEqual(mock_client.on_log, SUT._on_log)
+                mock_client.connect.assert_called_once_with(host, port, keepalive)
+
+    def test_run(self):
+        mock_logger = mock.Mock()
+        mock_client = mock.Mock()
+
+        with mock.patch('user.mqttreplicate.threading'):
+            with mock.patch('user.mqttreplicate.paho.mqtt'):
+
+                SUT = user.mqttreplicate.MQTTResponderLoopThread(mock_logger,
+                                                                 mock_client,
+                                                                 None,
+                                                                 None,
+                                                                 None,
+                                                                 None,
+                                                                 None,
+                                                                 None,
+                                                                 None,
+                                                                 None,
+                                                                 None)
+
+                SUT.run()
+
+                mock_client.loop_forever.assert_called_once_with()
+
+    def test_on_connect(self):
+        mock_logger = mock.Mock()
+        mock_client = mock.Mock()
+        topic = helpers.random_string()
+        qos = helpers.random_string()
+
+        with mock.patch('user.mqttreplicate.threading'):
+            with mock.patch('user.mqttreplicate.paho.mqtt'):
+
+                SUT = user.mqttreplicate.MQTTResponderLoopThread(mock_logger,
+                                                                 mock_client,
+                                                                 None,
+                                                                 None,
+                                                                 None,
+                                                                 None,
+                                                                 topic,
+                                                                 qos,
+                                                                 None,
+                                                                 None,
+                                                                 None)
+
+                mock_client.subscribe.return_value = (random.randint(1, 5), random.randint(6, 10))
+
+                SUT._on_connect(None)
+
+                mock_client.subscribe.assert_called_once_with(topic, qos)
+
+    def test_on_message(self):
+        mock_logger = mock.Mock()
+        mock_client = mock.Mock()
+
+        with mock.patch('user.mqttreplicate.threading'):
+            with mock.patch('user.mqttreplicate.paho.mqtt'):
+
+                SUT = user.mqttreplicate.MQTTResponderLoopThread(mock_logger,
+                                                                 mock_client,
+                                                                 None,
+                                                                 None,
+                                                                 None,
+                                                                 None,
+                                                                 None,
+                                                                 None,
+                                                                 None,
+                                                                 None,
+                                                                 None)
+
+                print(SUT.client_id)
+                print("done 1")
+
+        print("done 2")
+
     def test_template(self):
         mock_logger = mock.Mock()
         mock_client = mock.Mock()
@@ -22,19 +125,20 @@ class TestInit(unittest.TestCase):
         with mock.patch('user.mqttreplicate.threading'):
             with mock.patch('user.mqttreplicate.paho.mqtt'):
 
-                user.mqttreplicate.MQTTResponderLoopThread(mock_logger,
-                                                           mock_client,
-                                                           None,
-                                                           None,
-                                                           None,
-                                                           None,
-                                                           None,
-                                                           None,
-                                                           None,
-                                                           None,
-                                                           None)
+                SUT = user.mqttreplicate.MQTTResponderLoopThread(mock_logger,
+                                                                 mock_client,
+                                                                 None,
+                                                                 None,
+                                                                 None,
+                                                                 None,
+                                                                 None,
+                                                                 None,
+                                                                 None,
+                                                                 None,
+                                                                 None)
 
-        print("done 1")
+                print(SUT.client_id)
+                print("done 1")
 
         print("done 2")
 
